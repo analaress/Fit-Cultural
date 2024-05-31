4fb04ce87c67cddea22872b35ce89a9f7305565c
Ana <analaressads@gmail.com>
2024-05-31 16:00:05 -0300

    alterações

--- core/settings.py
+++ core/settings.py
@@ -118,10 +118,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'core/static',
-
+    BASE_DIR / 'base_static',
 ]
-STATIC_URL_ROOT = 'static_base'
+
 STATIC_ROOT = BASE_DIR / 'static'
 
 
